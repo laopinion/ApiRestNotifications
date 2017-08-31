7194e743145242f47cac7f410918269b9bfbd621
John serrano <andrey_201015@hotmail.com>
2017-08-31 11:25:04 -0500

    Terminando api rest notifications con create de token y send of messasing

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
 const config = require('./config');
 const admin = require("firebase-admin");
 
@@ -10,6 +9,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Documentación https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Documentación -> https://firebase.google.com/docs/cloud-messaging/admin/manage-topic-subscriptions
 const serviceAccount = require('./serviceAccountKey.json');
@@ -21,11 +27,7 @@ admin.initializeApp({
 
 app.get('/createToken/:token', function(req, res){
   // console.log('Create token post ', req.params.token);
-  const currentToken = req.params.token.slice(6);
-  // console.log('Create token post ', currentToken);
-
-  // console.log('Key -> ', config.key);
-
+  const currentToken = req.params.token;
   const topic = 'newnews';
 
   admin.messaging().subscribeToTopic(currentToken, topic)
@@ -33,34 +35,31 @@ app.get('/createToken/:token', function(req, res){
       // See the MessagingTopicManagementResponse reference documentation
       // for the contents of response.
       console.log("Successfully subscribed to topic:", response);
-      res.status(200).send({ message: 'response data ', data});
+      res.status(200).send({ message: 'response data ', response});
     })
     .catch(function(error) {
       console.log("Error subscribing to topic:", error);
       res.status(500).send({ message: 'response data ', error});
     });
+});
 
-  // const url = 'https://iid.googleapis.com/iid/v1/'+currentToken+'/rel/topics/newnews';
-  
-  // fetch(url, {
-  //   'method': 'POST',
-  //   'headers': {
-  //     'Authorization': 'key=' + config.key,
-  //     'Content-Type': 'application/json'
-  //   }
-  // })
-  // .then(function(response){
-  //   return response.json();
-  // })
-  // .then(function(data){
-  //   console.log(data);
-  //   res.status(200).send({ message: 'response data ', data});
-  // })
-  // .catch(function(error) {
-  //   console.error(error);
-  //   res.status(500).send({ message: 'response data ', error});
-  // })
-
+// Documentación https://firebase.google.com/docs/cloud-messaging/admin/send-messages
+app.post('/sendMessanging', function(req, res){
+  const topic = 'newnews';
+  console.log('data post -> ', req.body);
+  // res.status(200).send({ message: 'response data '});
+  const payload = req.body;
+  admin.messaging().sendToTopic(topic, payload)
+    .then(function(response) {
+      // See the MessagingTopicResponse reference documentation for the
+      // contents of response.
+      console.log("Successfully sent message:", response);
+      res.status(200).send({ message: 'response data ', response});
+    })
+    .catch(function(error) {
+      console.log("Error sending message:", error);
+      res.status(500).send({ message: 'response data ', error});
+    });
 });
 
 module.exports = app;
