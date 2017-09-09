2ba1e09fa9c461dc68cdeee3d3162a818b0aaae5
John serrano <andrey_201015@hotmail.com>
2017-09-09 09:22:31 -0500

    Agregando dotenv para manejar las variables de entorno de la conexion a la database y los la key para los json web token

--- services/index.js
+++ services/index.js
@@ -3,6 +3,9 @@
 const jwt = require('jwt-simple');
 const moment = require('moment');
 const config = require('../config');
+// Con esto creamos un file .env donde estan mis datos de conexión como variables de entorno.
+require('dotenv').config();
+
 //Generar ids unicos
 const uuidv1 = require('uuid/v1');
 
@@ -14,14 +17,14 @@ function createToken(user){
     exp: moment().add(1, 'year').unix(),
   }
   // Con moment add indicamos en que tiempo la fecha va caducar lo habitual son 14 days
-  return jwt.encode(payload, config.SECRET_TOKEN);
+  return jwt.encode(payload, process.env.JWT);
 }
 
 function decodeToken(token){
   // Las Promise se usan apartir de ES6 o ES2015 que es lo mismo
   const decoded = new Promise((resolve, reject) => {
     try{
-      const payload = jwt.decode(token, config.SECRET_TOKEN);
+      const payload = jwt.decode(token, process.env.JWT);
 
       if(payload.exp <= moment().unix()){
         reject({
